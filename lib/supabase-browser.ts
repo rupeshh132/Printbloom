f9fe17a0a2d04c89e8bd4c64778486192695bc66
Rupesh Vishwakarma <vrupesh132@gmail.com>
2026-09-08 03:41:55 +0530

    fix(auth): add middleware to refresh session and fix multiple gotrue instances

--- lib/supabase-browser.ts
+++ lib/supabase-browser.ts
@@ -1,15 +1,19 @@
 import { createBrowserClient } from "@supabase/ssr"
-import { SupabaseClient } from "@supabase/supabase-js"
-
-let supabase: SupabaseClient | undefined
 
 export function createSupabaseBrowserClient() {
-  if (supabase) return supabase
+  if (typeof window === 'undefined') {
+    return createBrowserClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    )
+  }
 
-  supabase = createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  )
-  
-  return supabase
+  if (!(window as any).__supabaseClient) {
+    (window as any).__supabaseClient = createBrowserClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    )
+  }
+
+  return (window as any).__supabaseClient
 }
